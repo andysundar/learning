20a2abe75b48e11ff96731ac33ec8b14e6517a25
Bandopadhyay,Anindya <Anindya.Bandopadhyay@Cerner.com>
2022-08-04 19:56:41 +0530

    Continuation experimental example Added examples to create virtual thread

--- JdkNewFatures/src/main/java/com/andy/learning/jdk20/VirtualThreadCharacteristicsExample.java
+++ JdkNewFatures/src/main/java/com/andy/learning/jdk20/VirtualThreadCharacteristicsExample.java
@@ -1,14 +1,36 @@
 package com.andy.learning.jdk20;
 
 
+import java.util.concurrent.Executors;
+
 public class VirtualThreadCharacteristicsExample {
 
     public static void main(String[] args) throws InterruptedException {
-        var virtualThread = Thread
+        var virtualThread1 = Thread
                 .ofVirtual()
                 .unstarted(()-> System.err.println(Thread.currentThread()));
-        virtualThread.start();
-        virtualThread.join();
-        System.err.println("Class Name :- " + virtualThread.getClass());
+        virtualThread1.start();
+
+        var virtualThread2 = Thread.startVirtualThread(()-> System.err.println(Thread.currentThread()));
+        var virtualThread3 = Thread.ofVirtual().start(()-> System.err.println(Thread.currentThread()));
+
+        // Virtual thread created with a factory
+        var factory = Thread.ofVirtual().factory();
+        Thread virtualThreadFromAFactory = factory.newThread(()-> System.err.println(Thread.currentThread()));
+        virtualThreadFromAFactory.start();
+
+        Executors.newVirtualThreadPerTaskExecutor();
+        Executors.newThreadPerTaskExecutor(factory); // Same as newVirtualThreadPerTaskExecutor
+        Executors.newSingleThreadExecutor(factory);
+        Executors.newCachedThreadPool(factory);
+        Executors.newFixedThreadPool(1, factory);
+        Executors.newScheduledThreadPool(1, factory);
+        Executors.newSingleThreadScheduledExecutor(factory);
+
+
+        virtualThread1.join();
+        virtualThread2.join();
+        virtualThread3.join();
+        System.err.println("Class Name :- " + virtualThread1.getClass());
     }
 }
